2126e16fe54ff3506386aecd6c14eedf1d782059
Santiago <chagovelez1@hotmail.com>
2017-05-13 11:48:37 -0500

    StandardCube working, some validations missing

--- app/Cube.php
+++ app/Cube.php
@@ -3,38 +3,72 @@
 namespace App;
 
 class Cube {
-  
-  private $matix;
+
+  private $matrix;
   private $validation_enabled;
-  
-  function __construct($size,$validate=false) {
-    for ($x = 0; $x <= $size - 1; $x++) {
-      for ($y = 0; $y <= $size - 1; $y++) {
-        for ($z = 0; $z <= $size - 1; $z++) {
+  private $N;
+
+  function __construct($size, $validate=false) {
+    if ($validate=TRUE) {
+      $this->validateSize($size);
+    }
+    for ($x=0; $x<=$size-1; $x++) {
+      for ($y=0; $y<=$size-1; $y++) {
+        for ($z=0; $z<=$size-1; $z++) {
           $matrix[$x][$y][$z]=0;
         }
       }
     }
-    $this->matix = $matrix;
-    $this->validation_enabled = $validate;
+    $this->matrix=$matrix;
+    $this->validation_enabled=$validate;
+    $this->N = $size;
   }
-  
-  public function update($x,$y,$z,$val){
-    $this->matix[$x-1][$y-1][$z-1] = (float)$val ;
+
+  public function update($x, $y, $z, $val) {
+    if($this->validation_enabled){
+      $this->validateUpdate($x, $y, $z, $val);
+    }
+    $this->matrix[$x-1][$y-1][$z-1]=(float)$val;
   }
-  
-  public function query($x1,$y1,$z1,$x2,$y2,$z2){
-    $result = 0;
-    for ($x = $x1-1 ; $x <= $x2-1; $x++) {
-      for ($y = $y1-1 ; $y <= $y2-1; $y++) {
-        for ($z = $z1-1 ; $z <= $z2-1; $z++) {
+
+  public function query($x1, $y1, $z1, $x2, $y2, $z2) {
+    if($this->validation_enabled){
+      $this->validateQuery($x1, $y1, $z1, $x2, $y2, $z2);
+    }
+    $result=0;
+    for ($x=$x1-1; $x<=$x2-1; $x++) {
+      for ($y=$y1-1; $y<=$y2-1; $y++) {
+        for ($z=$z1-1; $z<=$z2-1; $z++) {
           $result += $this->matrix[$x][$y][$z];
         }
       }
     }
     return $result;
   }
+
+  private function validateSize($size) {
+    if ($size<1 or 100<$size) {
+      abort(400, 'Cube size must be between 1 and 100, you input '.$size);
+    }
+  }
   
+  private function validateUpdate($x, $y, $z, $val) {
+    if ($x < 1 or $this->N < $x) {
+      abort(400, 'the X parameters should be between 1 and '.$this->N.' you input '.$x);
+    }
+    if ($y < 1 or $this->N < $y) {
+      abort(400, 'the Y parameters should be between 1 and '.$this->N.' you input '.$y);
+    }
+    if ($z < 1 or $this->N < $z) {
+      abort(400, 'the Z parameters should be between 1 and '.$this->N.' you input '.$z);
+    }
+    if ($val < 1 or $this->N < $val) {
+      abort(400, 'the VAl pameters for update should be between 1 and '.$$this->N.' you input '.$val);
+    }
+  }
   
+  private function validateQuery($x1, $y1, $z1, $x2, $y2, $z2) {
+    
 }
 
+}
--- app/Http/Controllers/StandarCubeController.php
+++ app/Http/Controllers/StandarCubeController.php
@@ -20,25 +20,57 @@ class StandarCubeController extends Controller {
 
     //loop thrhug test cases
     $rows_to_jum=1;
+    $results = '';
     for ($test_index=1; $test_index<=$T; $test_index++) {
       $N_M=explode(' ', $commands[$rows_to_jum]);
       $N=$N_M[0];
       $M=$N_M[1];
       $this->validation('M', ['M'=>$M]);
-      $Cube = new Cube($N,TRUE);
-      dd($Cube);
+      $cube = new Cube($N,TRUE);
+      $operations = array_slice($commands, $rows_to_jum+1, $M);
+      $results .= $this->perform_operations($cube, $operations);
+      $rows_to_jum += $M+1;
     }
+    return $results;
+  }
+  
+  private function perform_operations(Cube $cube,$operations) {
+    $results = '';
+    foreach ($operations as $op){
+      $params = explode(' ', $op);
+      if(!count($params)>1){
+            abort(400,' Empty operation, opertions must start with "UPDATE" or "QUERY" string');
+      }
+      switch ($params[0]){
+        case 'UPDATE':
+          if(count($params)!= 5){
+            abort(400,'An UPDATE operation must have 5 params, check you inputs and try again');
+          }
+          $cube->update($params[1], $params[2], $params[3], $params[4]);
+          break;
+        case 'QUERY':
+          if(count($params)!= 7){
+            abort(400,'A QUERY operation must have 7 params, check you inputs and try again');
+          }
+          $results .= $cube->query($params[1], $params[2], $params[3], $params[4], $params[5], $params[6]);
+          break;
+        default :
+          abort(400,'you input a non valid operation:'. $params[0]);
+          break;
+      }
+    }
+    return $results;
   }
 
   private function validation($type, $arr_vals) {
     switch ($type) {
       case 'T':
-        if ($arr_vals['T']<=1 or 50<=$arr_vals['T']) {
+        if ($arr_vals['T']<1 or 50<$arr_vals['T']) {
           return abort(400, 'T must be between 1 and 50, you input '.$arr_vals['T']);
         }
         break;
       case 'M':
-        if ($arr_vals['M']<=1 or 1000<=$arr_vals['M']) {
+        if ($arr_vals['M']<1 or 1000<$arr_vals['M']) {
           return abort(400, 'M must be between 1 and 1000, you input '.$arr_vals['M']);
         }
         break;
